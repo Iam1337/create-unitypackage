664c55f1cd7d67db08ef306636ebb79339c675c3
Vladimir Sigalkin <_ext_@bk.ru>
2022-01-07 15:52:28 +0300

    internal commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const verifyPackage = require("./lib/verify");
+const preparePackage = require("./lib/prepare");
+
+let verified;
+
+async function verifyConditions(pluginConfig, context) {
+    const { options } = context;
+    if (options.prepare) {
+        const preparePlugin = castArray(options.prepare).find(config => config.path && config.path === "@iam1337/create-unitypackage") || {};
+
+        pluginConfig.packageRoot = defaultTo(pluginConfig.packageRoot, preparePlugin.packageRoot);
+        pluginConfig.projectRoot = defaultTo(pluginConfig.projectRoot, preparePlugin.projectRoot);
+        pluginConfig.output = defaultTo(pluginConfig.output, preparePlugin.output);
+    }
+
+    verifyPackage(pluginConfig, context);
+    verified = true;
+}
+
+async function prepare(pluginConfig, context) {
+    if (!verified) {
+        verifyPackage(pluginConfig, context);
+        verified = true;
+    }
+
+    preparePackage(pluginConfig, context);
+}
+
+module.exports = { verifyConditions, prepare };
